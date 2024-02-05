0106217b9a370d2c0df45c9599af832212683b78
MUHMMAD SALEH <khanmohdsaleh2002@gmail.com>
2024-02-05 23:55:03 +0530

    complete feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ImageSlider from './ImageSlider';
 
 function App() {
   return (
     <div className="App">
-      
+      <ImageSlider/>
     </div>
   );
 }
